cfe112976a5da5823b705c53f5635ee93fbfd08f
quzehu <quzehu78@163.com>
2021-01-07 17:58:49 +0800

    第二阶段需求增加注释

--- src/main/java/com/quzehu/learn/api/IfOrElse.java
+++ src/main/java/com/quzehu/learn/api/IfOrElse.java
@@ -10,23 +10,56 @@ import java.util.Objects;
  * @Version 1.0
  */
 public interface IfOrElse {
+
     /**
      * 执行是否相等判断，并执行对应的动作
      * @Date 2021/1/7 17:35
      * @param v1 参数1
      * @param v2 参数2
-     * @param equalsAction 相等的动作
-     * @param notEqualsAction 不相等的动作
+     * @param eAction 相等的动作
+     * @param neAction 不相等的动作
      * @Author Qu.ZeHu
      * @return void
      **/
-    default <T> void ifPresentOrElse(T v1, T v2, Runnable equalsAction, Runnable notEqualsAction) {
+    default <T> void ifPresentOrElse(T v1, T v2, Runnable eAction, Runnable neAction) {
         Objects.requireNonNull(v1);
         if (v1.equals(v2)) {
-            equalsAction.run();
+            eAction.run();
         } else {
-            notEqualsAction.run();
+            neAction.run();
         }
     }
+    /**
+     * 执行是否相等判断，并执行对应的动作
+     * @Date 2021/1/7 17:55
+     * @param v1 参数1
+     * @param v2 参数2
+     * @param eAction 相等的动作
+     * @Author Qu.ZeHu
+     * @return void
+     **/
+    default <T> void ifPresent(T v1, T v2, Runnable eAction) {
+        Objects.requireNonNull(v1);
+        if (v1.equals(v2)) {
+            eAction.run();
+        }
+    }
+
+    /**
+     * 执行是否相等判断，并执行对应的动作
+     * @Date 2021/1/7 17:56
+     * @param v1 参数1
+     * @param v2 参数2
+     * @param neAction 不相等的动作
+     * @Author Qu.ZeHu
+     * @return void
+     **/
+    default <T> void orElse(T v1, T v2, Runnable neAction) {
+        Objects.requireNonNull(v1);
+        if (!v1.equals(v2)) {
+            neAction.run();
+        }
+    }
+
 
 }
